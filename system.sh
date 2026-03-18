e86029955ecd77a799fa71497ffe20704201cef5
Ubuntu <Saiguvvala25@gmail.com>
2026-03-18 19:59:54 +0000

    New file

---
+++ system.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "System Information"
+echo "------------------"
+echo "Hostname: $(hostname)"
+echo "Current User: $(whoami)"
+echo "Current Date: $(date)"
+echo "Memory Usage:"
+free -h
+echo "Current time"
+
+
+
